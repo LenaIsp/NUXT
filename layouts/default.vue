1b4065432f0fa42b71e0100a7700719f3e011097
Elena Ispravnikova <elena-isp@192.168.0.84>
2023-09-26 18:17:14 +0300

    NuxtLayer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
+  <div id="default-layout">
     <PartsHeader/>
       <div class="wrap-top">
-        <NuxtPage />
+        <slot />
       </div>
     <PartsFooter/>
   </div>
